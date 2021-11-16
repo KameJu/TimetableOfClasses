987d91b66674057ee7069a8393755b01554f81e3
KameJu <sasiskasssss6052@gmail.com>
2021-11-16 15:54:04 +0200

    Initial commit

---
+++ src/main/java/TZ/TimetableOfClasses/TimetableOfClassesApplication.java
@@ -0,0 +1,13 @@
+package TZ.TimetableOfClasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableOfClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableOfClassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TZ/TimetableOfClasses/TimetableOfClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package TZ.TimetableOfClasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableOfClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
